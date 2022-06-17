2917f2c7373af26f6cc4f02d8d127577fe990f38
FauconSpartiate <49079695+FauconSpartiate@users.noreply.github.com>
2022-06-17 11:12:15 +0200

    Fix build warnings

--- src/services/weather.js
+++ src/services/weather.js
@@ -2,6 +2,8 @@ const axios = require("axios");
 
 export default {
   async getCurrentWeather() {
-    return await axios.get("https://foxi.ltam.lu/2CI/pirda350/public/OmniWeather/getLatest.php");
+    return await axios.get(
+      "https://foxi.ltam.lu/2CI/pirda350/public/OmniWeather/getLatest.php"
+    );
   },
 };
