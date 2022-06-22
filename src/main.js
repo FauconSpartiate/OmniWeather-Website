d8879fdb7b558748bbf3946fd7e6bf22d1eab456
Faucon <49079695+FauconSpartiate@users.noreply.github.com>
2022-06-22 10:51:01 +0200

    Fixed form and performance

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import Vue from "vue";
-import App from "./App.vue";
 import "./registerServiceWorker";
+import App from "./App.vue";
+import Vue from "vue";
 import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
--- src/services/weather.js
+++ src/services/weather.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 export default {
   async getCurrentWeather() {
-    return await axios.get(
+    return axios.get(
       "https://foxi.ltam.lu/2CI/pirda350/public/OmniWeather/getLatest.php"
     );
   },
